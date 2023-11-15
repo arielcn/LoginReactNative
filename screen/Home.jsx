c74dd6d3dc54378c58c2a94b891d07cf12c2f645
ARIEL DAN COHEN <46914677@ort>
2023-11-15 09:05:16 -0300

    api para proyecto final

--- screen/Home.jsx
+++ screen/Home.jsx
@@ -1,16 +1,19 @@
+import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { ImageBackground, Button, StyleSheet, View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { getAuth } from "firebase/auth";
 import { collection, doc, getDoc, getFirestore } from "firebase/firestore";
 
-const image = { uri: 'https://i.pinimg.com/originals/fa/25/ef/fa25ef6cc85d2ea64278c7c09887943f.jpg' };
+const image = { uri: 'https://global-uploads.webflow.com/5ca5fe687e34be0992df1fbe/6235ea7fbaf601e8d3980228_boy-kicking-ball-on-football-field-2021-09-24-03-47-56-utc-min-min.jpg' };
 
 function Home() {
   const navigation = useNavigation();
   const [userData, setUserData] = useState('')
 
-  useEffect(() => {
+  const baseUrl = 'https://reqres.in';
+
+  useEffect(async () => {
     console.log('entrasT?')
     const fetchDataFirestore = async () => {
       const auth = getAuth();
@@ -27,22 +30,40 @@ function Home() {
       }
     }
     fetchDataFirestore();
+
+    const options = {
+      method: 'GET',
+      url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
+      params: {date: '2021-01-29'},
+      headers: {
+        'X-RapidAPI-Key': '8251816370msh5a663ad8dd640b4p1fe4b9jsnf7a487bd8d54',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+
+    } catch (error) {
+      console.error(error);
+    }
   }, [])
 
 
   return (
-      <View style={styles.container}>
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <Text style={styles.title}>¡BIENVENIDO, {userData.nombre}!</Text>
-          <Button
-            style={styles.botonPerfil}
-            onPress={() => {
-              navigation.navigate("EditarPerfil");
-            }}
-            title="Editar perfil"
-          />
-        </ImageBackground>
-      </View>
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.title}>¡BIENVENIDO, {userData.nombre}!</Text>
+        <Button
+          style={styles.botonPerfil}
+          onPress={() => {
+            navigation.navigate("EditarPerfil");
+          }}
+          title="Editar perfil"
+        />
+      </ImageBackground>
+    </View>
   );
 }
 
